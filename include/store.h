fe2abe53aed0f0e977d8973e7e5408e09a6692a8
nguyenduybao1 <nguyenbao7795@gmail.com>
2026-01-25 12:00:46 +0000

    feat: add TTL support with lazy expiration

--- include/store.h
+++ include/store.h
@@ -3,12 +3,21 @@
 #include <string>
 #include <unordered_map>
 #include <shared_mutex>
+#include <chrono>
+
+struct Entry{
+    std::string value;
+    std::optional<std::chrono::steady_clock::time_point> expire_at;
+};
+
 class KVStore{
     public: 
         void set(const std::string& key, const std::string& value);
+        void set(const std::string& key, const std::string& value, std::chrono::seconds ttl);
+
         std::optional<std::string> get(const std::string& key);
         void del(const std::string& key);
     private:
-        std::unordered_map<std::string, std::string> data;
+        std::unordered_map<std::string, Entry> data;
         mutable std::shared_mutex m;
 };
--- src/store.cpp
+++ src/store.cpp
@@ -1,22 +1,38 @@
-#include <store.h>
+#include "store.h"
 #include <shared_mutex>
 #include <mutex>
 
-void KVStore::set(const std::string& key, const std::string& value){
+using Clock = std::chrono::steady_clock;
+
+void KVStore::set(const std::string& key, const std::string& value) {
     std::unique_lock<std::shared_mutex> lock(m);
-    data[key] = value;
+    data[key] = {value, std::nullopt};
 }
 
-std::optional<std::string> KVStore::get(const std::string& key){
-    std::shared_lock<std::shared_mutex> lock(m);
+void KVStore::set(const std::string& key,
+                  const std::string& value,
+                  std::chrono::seconds ttl) {
+    std::unique_lock<std::shared_mutex> lock(m);
+    data[key] = {value, Clock::now() + ttl};
+}
+
+std::optional<std::string> KVStore::get(const std::string& key) {
+    std::unique_lock<std::shared_mutex> lock(m); 
+
     auto it = data.find(key);
-    if(it != data.end()){
-        return it->second;
+    if (it == data.end())
+        return std::nullopt;
+
+    if (it->second.expire_at &&
+        Clock::now() >= *(it->second.expire_at)) {
+        data.erase(it);
+        return std::nullopt;
     }
-    return std::nullopt;
+
+    return it->second.value;
 }
 
-void KVStore::del(const std::string& key){
+void KVStore::del(const std::string& key) {
     std::unique_lock<std::shared_mutex> lock(m);
     data.erase(key);
 }
